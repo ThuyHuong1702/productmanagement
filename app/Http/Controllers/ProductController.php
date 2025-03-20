cba3ccd7be5eda6596233fdf5a71187e4adf4624
ThuyHuong1702 <dothuyhuong90@gmail.com>
2025-03-20 11:44:28 +0700

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
 use App\Models\Product;
-
+use Illuminate\Validation\ValidationException;//thêm
 
 class ProductController extends Controller
 {
