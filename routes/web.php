80343d58fbe15044947a078c56d15febd74e86a7
ThuyHuong1702 <dothuyhuong90@gmail.com>
2025-03-20 11:13:05 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
 Route::resource('products', ProductController::class);
+//thêm
+use App\Http\Controllers\Admin\ProductController;
+
+Route::post('/admin/products/store', [ProductController::class, 'store']);
+
